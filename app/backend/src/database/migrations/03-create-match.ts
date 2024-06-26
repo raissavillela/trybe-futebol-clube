bd59d5306c3fd4fd9fa6a88eccece79daa8e1f43
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-06-26 12:33:46 -0700

    feat: Fluxos

--- app/backend/src/database/migrations/03-create-match.ts
+++ app/backend/src/database/migrations/03-create-match.ts
@@ -13,30 +13,35 @@ module.exports = {
       homeTeamId: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        field: "home_team_id",
         references: {
-          model: 'Teams',
+          model: 'teams',
           key:
            'id'
         }
       },
       homeTeamGoals: {
         type: DataTypes.INTEGER,
+        field: "home_team_goals",
         allowNull: false
       },
       awayTeamId: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        field: "away_team_id",
         references: {
-          model: 'Teams',
+          model: 'teams',
           key: 'id'
         }
       },
       awayTeamGoals: {
         type: DataTypes.INTEGER,
+        field: "away_team_goals",
         allowNull: false
       },
       inProgress: {
         type: DataTypes.BOOLEAN,
+        field: "in_progress",
         allowNull: false
       }
     });
